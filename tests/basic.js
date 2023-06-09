a4a09327ffbccf3a6c7bfba12f991c2f80a34057
Brad Marsden <silentbot1@gmail.com>
2023-06-09 01:01:02 +0100

    update manual tests

--- tests/basic.js
+++ tests/basic.js
@@ -3,24 +3,28 @@ import NatAPI from '../index.js'
 const port = 6690
 
 const enablePMP = [true, false]
+const enableUPNP = [true, false]
 const protocols = ['TCP', 'UDP']
 
 const test = async (protocol, opts) => {
-  const client = new NatAPI({ enablePMP: opts.enablePMP })
+  const client = new NatAPI({ enablePMP: opts.enablePMP, enableUPNP: opts.enableUPNP })
 
   const options = { publicPort: port, privatePort: port, protocol }
-  await client.map(options)
-  console.log(`Port ${port} mapped to ${port} ${protocol} via ${opts.enablePMP ? 'PMP' : 'UPnP'}`)
-  await client.unmap(options)
-  console.log(`Port ${port} unmapped from ${port} ${protocol} via ${opts.enablePMP ? 'PMP' : 'UPnP'}`)
+  let response = await client.map(options)
+  console.log(`   Port ${port} mapped to ${port} ${protocol} Success: ${response}`)
+  response = await client.unmap(options)
+  console.log(`   Port ${port} unmapped from ${port} ${protocol} Success: ${response}`)
 
   await client.destroy()
 }
 
 const main = async () => {
-  for (const usePMP of enablePMP) {
-    for (const protocol of protocols) {
-      await test(protocol, { enablePMP: usePMP })
+  for (const useUPNP of enableUPNP) {
+    for (const usePMP of enablePMP) {
+      console.log(`enablePMP: ${usePMP} enableUPNP: ${useUPNP}`)
+      for (const protocol of protocols) {
+        await test(protocol, { enablePMP: usePMP, enableUPNP: useUPNP })
+      }
     }
   }
 }
